4e1cb5e538d1a9f2ab4b36e253b796532207282a
James Pancoast <jamesp@jpancoast.dev>
2025-05-19 21:42:07 -0600

    [main] removed the ssh key stuff

--- droplets/tailscale/main.tf
+++ droplets/tailscale/main.tf
@@ -6,17 +6,17 @@ resource "digitalocean_project" "tailscale" {
   environment = var.project_environment
 }
 
-resource "digitalocean_ssh_key" "tailscale" {
-  name       = local.do_ssh_key_name
-  public_key = file(var.public_key_location)
-}
+#resource "digitalocean_ssh_key" "tailscale" {
+#  name       = local.do_ssh_key_name
+#  public_key = file(var.public_key_location)
+#}
 
 resource "digitalocean_droplet" "tailscale" {
-  image             = data.digitalocean_image.ubuntu.slug
-  name              = local.droplet_name
-  region            = var.droplet_region
-  size              = var.droplet_size
-  ssh_keys          = [digitalocean_ssh_key.tailscale.fingerprint]
+  image  = data.digitalocean_image.ubuntu.slug
+  name   = local.droplet_name
+  region = var.droplet_region
+  size   = var.droplet_size
+  #  ssh_keys          = [digitalocean_ssh_key.tailscale.fingerprint]
   graceful_shutdown = var.graceful_shutdown
 
   user_data = <<-EOF
--- droplets/tailscale/outputs.tf
+++ droplets/tailscale/outputs.tf
@@ -1,4 +1,16 @@
 
-output "start_tailscale" {
-  value = "ssh -o IdentitiesOnly=yes -i ${var.private_key_location} ${var.ssh_user}@${digitalocean_droplet.tailscale.ipv4_address}"
+#output "start_tailscale" {
+#  value = "ssh -o IdentitiesOnly=yes -i ${var.private_key_location} ${var.ssh_user}@${digitalocean_droplet.tailscale.ipv4_address}"
+#}
+
+output "digitalocean_project_tailscale" {
+  value = digitalocean_project.tailscale.id
+}
+
+output "digitalocean_droplet_tailscale" {
+  value = digitalocean_droplet.tailscale.id
+}
+
+output "digitalocean_project_resources_tailscale" {
+  value = digitalocean_project_resources.tailscale.id
 }
